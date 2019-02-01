50dbdcde511b7bc7b9ca9b766abdfb5181d14920
rostykusvasyl <rostykusvasyl@gmail.com>
2019-02-01 21:21:55 +0200

    Basic configuration of the provider.

---
+++ weatherapp/accu/config.py
@@ -0,0 +1,12 @@
+# AccuWeather provider reloaded configuration.
+ACCU_PROVIDER_NAME = 'accu'
+ACCU_PRVIDER_TITLE = 'AccuWeather'
+
+# The name of the city and the address for which we will display the weather
+# conditions by default for site accuweather.
+DEFAULT_ACCU_LOCATION_NAME = 'Brody'
+DEFAULT_ACCU_LOCATION_URL = ('https://www.accuweather.com/uk/ua/'
+                             'brody/324506/weather-forecast/324506')
+
+# Address for finding a place for which we will display weather conditions.
+ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
