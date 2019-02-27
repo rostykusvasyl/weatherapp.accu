5c650c092e9cc46dea2e016e8945e50530fb5b6c
rostykusvasyl <rostykusvasyl@gmail.com>
2019-02-27 21:09:51 +0200

    Added the ability to perform html parsing using regular expressions.

--- weatherapp/accu/provider.py
+++ weatherapp/accu/provider.py
@@ -2,6 +2,8 @@
 """
 
 import re
+import requests
+import html
 from bs4 import BeautifulSoup
 
 from weatherapp.accu import config
@@ -77,7 +79,65 @@ class AccuProvider(WeatherProvider):
 
         # find the <div> container with the information we need
         soup = BeautifulSoup(page, 'html.parser')
-        if not self.app.options.tomorrow:
+        if self.app.options.tomorrow == 'tomorrow':
+            tag_container = \
+                soup.find(class_="day hv cl")
+            if tag_container:
+                current_day_url = tag_container.find('a').attrs['href']
+                if current_day_url:
+                    current_day = self.get_page_source(current_day_url)
+                    current_day_page = BeautifulSoup(current_day,
+                                                     'html.parser')
+                    if current_day_page:
+                        weather_details = \
+                            current_day_page.find(id="detail-day-night")
+                        temp_info = weather_details.find('span',
+                                                         class_="large-temp")
+                        if temp_info:
+                            weather_info['temp'] = temp_info.get_text()
+                        realfeel = weather_details.find('span',
+                                                        class_="realfeel")
+                        if realfeel:
+                            weather_info['feels_like'] = realfeel.get_text()
+                        cond_info = weather_details.find(class_="cond")
+                        if cond_info:
+                            weather_info['cond'] = cond_info.get_text().strip()
+                        wind_direction_tag = \
+                            weather_details.find(class_="wind-stats")
+                        wind_speed_tag = weather_details.find("strong")
+                        if wind_direction_tag:
+                            weather_info['wind'] = wind_speed_tag.get_text()
+
+        elif self.app.options.regexp:
+            tag_container = \
+                re.compile(r"(?is)<li class=\"(day|night) current first cl\"[^>]*>(.+?)</li")
+            tag = tag_container.findall(page)
+            url = str(tag[0])
+            forecast = re.compile(r"(?is)<h3><a href=\" *(.+?)[?\">]")
+            f_info = forecast.findall(url)
+            current_day_url = f_info[0]
+            if current_day_url:
+                current_day = self.get_page_source(current_day_url)
+
+                temp_info = re.compile(r"(?is)<span class=\"large-temp\"[^>]*>(.+?)</span>")
+                temp = temp_info.findall(current_day)
+                weather_info['temp'] = html.unescape(temp[0].strip())
+
+                realfeel_info = re.compile(r"(?is)<span class=\"real-feel\"[^>]*>(.+?)</span>")
+                realfeel = realfeel_info.findall(current_day)
+                weather_info['feels_like'] = html.unescape(realfeel[0].strip())
+
+                cond_info = re.compile(r"(?is)<span class=\"cond\"[^>]*>(.+?)</span>")
+                cond = cond_info.findall(current_day)
+                weather_info['cond'] = cond[-1]
+
+                wind_info = re.compile(r"(?is)<li class=\"wind\"[^>]*>(.+?)</li>")
+                wind_i = wind_info.findall(current_day)
+                wind_details = re.compile(r"(?is)<li class=\"wind\"><strong>(.+?)</strong></li>")
+                wind_d = wind_details.findall(current_day)
+                weather_info['wind'] = wind_i[0].strip() + ' ' + wind_d[0]
+
+        else:
             tag_container = \
                 soup.find(class_=re.compile("(day|night) current first cl"))
             if tag_container:
@@ -108,33 +168,5 @@ class AccuProvider(WeatherProvider):
                             wind_speed = wind_speed_tag.get_text()
                             weather_info['wind'] = wind_direction + ' ' +\
                                 wind_speed
-        else:
-            tag_container = \
-                soup.find(class_="day hv cl")
-            if tag_container:
-                current_day_url = tag_container.find('a').attrs['href']
-                if current_day_url:
-                    current_day = self.get_page_source(current_day_url)
-                    current_day_page = BeautifulSoup(current_day,
-                                                     'html.parser')
-                    if current_day_page:
-                        weather_details = \
-                            current_day_page.find(id="detail-day-night")
-                        temp_info = weather_details.find('span',
-                                                         class_="large-temp")
-                        if temp_info:
-                            weather_info['temp'] = temp_info.get_text()
-                        realfeel = weather_details.find('span',
-                                                        class_="realfeel")
-                        if realfeel:
-                            weather_info['feels_like'] = realfeel.get_text()
-                        cond_info = weather_details.find(class_="cond")
-                        if cond_info:
-                            weather_info['cond'] = cond_info.get_text().strip()
-                        wind_direction_tag = \
-                            weather_details.find(class_="wind-stats")
-                        wind_speed_tag = weather_details.find("strong")
-                        if wind_direction_tag:
-                            weather_info['wind'] = wind_speed_tag.get_text()
 
         return weather_info
